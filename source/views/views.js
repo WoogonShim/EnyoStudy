c4653ec455b3c673274f136152614c46bcafb2a2
Woogon Shim <woogon.shim@lge.com>
2015-04-01 22:58:25 +0900

    Step 05) Create a panel for searching
    Signed-off-by: Woogon Shim <woogon.shim@lge.com>

--- source/views/views.js
+++ source/views/views.js
@@ -5,12 +5,27 @@
 */
 
 enyo.kind({
+	name: "flickr.SearchPanel",
+	kind: "moon.Panel",
+	title: "Search Flickr",
+	titleBelow: "Enter search term above",
+	headerOptions: {inputMode: true, dismissOnEnter: true},
+	handlers: {
+		onInputHeaderChange: "search"
+	},
+	search: function(inSender, inEvent) {
+		alert(inEvent.originator.get("value"));
+	},
+});
+
+enyo.kind({
 	name: "flickr.MainView",
 	classes: "moon enyo-fit",
 	kind: "enyo.Control",
 	components: [
-		{kind: "moon.Panels", classes: "enyo-fit", pattern: "alwaysviewing", popOnBack: true, components: [
-			{kind: "moon.Panel", title: "First Panel"}
+		{kind: "moon.Panels", classes: "enyo-fit", pattern: "alwaysviewing",
+			popOnBack: true, components: [
+			{kind: "flickr.SearchPanel"}
 		]}
 	]
 });
